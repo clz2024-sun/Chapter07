8a44ea4b7a3ec4ccdc9069c0916b161a416fad8b
clz2024.sun <clz2024.sun@gmail.com>
2024-07-16 16:44:40 +0900

    2024-07-16

--- src/echo/Server.java
+++ src/echo/Server.java
@@ -24,57 +24,30 @@ public class Server {
 		System.out.println("<서버시작>");
 		System.out.println("==================================================");
 		
-		
-		System.out.println("[연결을 기다리고 있습니다.]");
-		
-		
-		//클라이언트가 연결되면 accept()가실행되고 소켓이생긴다
-		//socket은 종이컵 전화기
-		Socket socket = serverSocket.accept(); 
-		
-////////////
-		System.out.println("[클라이언트가 연결 되었습니다]");
-		
-		//읽기 스트림
-		InputStream in = socket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-		
-		//쓰기 스트림
-		OutputStream out = socket.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
-		
-		
+		//반복
 		while(true) {
-
-			//메세지 받기
-			String msg = br.readLine();
+	
+			System.out.println("[연결을 기다리고 있습니다.]");
+			//클라이언트가 연결되면 accept()가실행되고 소켓이생긴다
+			//socket은 종이컵 전화기
+			Socket socket = serverSocket.accept(); 
 			
-			if(msg == null) {
-				break;
-			}
-			
-			System.out.println("받은메세지:" + msg);
-			
-			//메세지 보내기
-			bw.write(msg);
-			bw.newLine();
-			bw.flush();
+			Thread thread =  new ServerThread(socket);
+			thread.start();
 			
 		}
-////////////		
 		
-		
+		/*
 		System.out.println("=====================================");
 		System.out.println("<서버 종료>");
 		
 		//닫기
+		
 		br.close();
 		bw.close();
 		socket.close();
 		serverSocket.close();
-		
+		*/
 	}
 
 }
--- src/echo/ServerThread.java
+++ src/echo/ServerThread.java
@@ -2,57 +2,70 @@ package echo;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.net.Socket;
 
 //출장가서 일해야함
 public class ServerThread extends Thread {
 
 	//필드
+	private Socket socket;
 	
 	//생성자
+	public ServerThread(Socket socket) {
+		this.socket = socket;
+	}
+	
 	
 	//메소드 gs
 	
 	//메소드 일반
 	@Override
 	public void run() {
-		System.out.println("[클라이언트가 연결 되었습니다]");
 		
-		//읽기 스트림
-		InputStream in = socket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
-		BufferedReader br = new BufferedReader(isr);
-		
-		//쓰기 스트림
-		OutputStream out = socket.getOutputStream();
-		OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
-		BufferedWriter bw = new BufferedWriter(osw);
-		
-		
-		while(true) {
-
-			//메세지 받기
-			String msg = br.readLine();
+		try {
 			
-			if(msg == null) {
-				break;
+			System.out.println("[클라이언트가 연결 되었습니다]");
+			
+			//읽기 스트림
+			InputStream in = socket.getInputStream();
+			InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+			
+			//쓰기 스트림
+			OutputStream out = socket.getOutputStream();
+			OutputStreamWriter osw = new OutputStreamWriter(out, "UTF-8");
+			BufferedWriter bw = new BufferedWriter(osw);
+			
+			
+			while(true) {
+
+				//메세지 받기
+				String msg = br.readLine();
+				
+				if(msg == null) {
+					break;
+				}
+				
+				System.out.println("받은메세지:" + msg);
+				
+				//메세지 보내기
+				bw.write(msg);
+				bw.newLine();
+				bw.flush();
+				
 			}
 			
-			System.out.println("받은메세지:" + msg);
-			
-			//메세지 보내기
-			bw.write(msg);
-			bw.newLine();
-			bw.flush();
-			
+		}catch(IOException e) {
+			System.out.println(e.toString());
 		}
 		
 		
 	}
-	
-	
+
 
 }
